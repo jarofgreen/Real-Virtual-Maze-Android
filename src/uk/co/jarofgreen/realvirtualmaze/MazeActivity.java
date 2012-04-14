1777d862758af759c11d15414e9c009577ef46fb
James <james@jarofgreen.co.uk>
2012-04-14 12:31:07 +0100

    Basically works in Android 2.1 but not Android 4!

--- src/uk/co/jarofgreen/realvirtualmaze/MazeActivity.java
+++ src/uk/co/jarofgreen/realvirtualmaze/MazeActivity.java
@@ -1,13 +1,50 @@
 package uk.co.jarofgreen.realvirtualmaze;
 
+import java.util.List;
+
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.MapView.Projection;
+import org.osmdroid.views.overlay.Overlay;
+import org.osmdroid.views.util.constants.MapViewConstants;
+
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Paint.Style;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 public class MazeActivity extends Activity {
+	
+
+    private MapView mapView;
+    private MapController mapController;
+    private LocationManager mLocMgr;
+	
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+
+        mapView = (MapView) this.findViewById(R.id.mapview);
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+        mapView.setBuiltInZoomControls(true);
+        mapView.setMultiTouchControls(true);
+
+
+        //mapView.invalidate();
+
+        
     }
 }
